31a8760dd030e01d34e0c7a906815e0b9ee70b61
mohammad_mohsen <mohammad_mohsen@siliconexpert.com>
2016-02-21 22:33:32 +0200

    Specify method output format as JSON

--- src/main/java/com/jarmy/lab/spring/rest/MyAPI.java
+++ src/main/java/com/jarmy/lab/spring/rest/MyAPI.java
@@ -1,12 +1,13 @@
 package com.jarmy.lab.spring.rest;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyAPI {
 
-    @RequestMapping(value = "/test")
+    @RequestMapping(value = "/test", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public MyUser hello() {
         MyUser user = new MyUser();
         user.setName("Abbas");
